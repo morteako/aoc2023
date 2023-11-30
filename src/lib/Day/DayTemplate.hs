bfe5d7300f1d73b4ea454e65b167ad842d3369b8
Morten Kolstad <morten.kolstad@bekk.no>
2023-11-30 16:50:22 +0100

    add raw strings

--- src/lib/Day/DayTemplate.hs
+++ src/lib/Day/DayTemplate.hs
@@ -1,6 +1,7 @@
 module Day.DayTemplate (run) where
 
 import Test.HUnit ((@=?))
+import Text.RawString.QQ
 
 parse = id
 
@@ -8,10 +9,29 @@ solveA = id
 
 solveB = id
 
+testInput =
+  [r|
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+|]
+
 run :: String -> IO ()
-run xs = do
-  print xs
-  let parsed = parse xs
+run input = do
+  input <- putStrLn "#####    testInput   #####" >> pure testInput
+  print input
+  let parsed = parse input
   print parsed
   let resA = solveA parsed
   print resA
