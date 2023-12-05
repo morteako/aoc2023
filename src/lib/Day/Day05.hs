89bdc1b75593d33be71e35fe7a20c721a9c25ab4
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-05 17:42:37 +0100

    z3

--- src/lib/Day/Day05.hs
+++ src/lib/Day/Day05.hs
@@ -5,18 +5,21 @@
 module Day.Day05 (run) where
 
 import Control.Arrow
+import Control.Lens
 import Control.Monad (void)
 import Data.Function
+import Data.IntMap (IntMap)
+import Data.IntMap qualified as Map
 import Data.IntSet qualified as Set
-import Data.IntervalMap (Interval (..))
-import Data.IntervalMap.Lazy qualified as IM
 import Data.List
 import Data.List qualified as List
 import Data.List.Extra (firstJust, minimumOn)
 import Data.List.Split (splitOn)
 import Data.Map (Map)
-import Data.Map qualified as Map
 import Data.Semigroup
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.IO qualified as Text
 import Debug.Trace
 import Safe (readMay)
 import Test.HUnit ((@=?))
@@ -51,8 +54,33 @@ readInt x = case readMay x of
 
 data Func = Func {adder :: Int, min :: Int, max :: Int} deriving (Show)
 
+-- newtype Adder = Adder Int deriving (Show)
+
+-- asdf
 mapToFunc :: Mapping -> Func
 mapToFunc m = Func (m.destStart - m.sourceStart) m.sourceStart (m.sourceStart + m.range)
+
+type M = IntMap Int
+
+funcToM :: Func -> M
+funcToM f = Map.singleton f.min (f.adder)
+
+combMap from to = Map.mapWithKey (\k v -> Map.findWithDefault v k to)
+
+par x = "(" ++ x ++ ")"
+
+pack = Text.pack . show
+
+toZ3 :: Text -> Text -> Func -> Text
+toZ3 oldId newId f =
+  repl "#MIN" (pack f.min) $
+    repl "#MAX" (pack f.max) $
+      repl "#NEWID" newId $
+        repl "#OLDID" oldId $
+          repl "#ADDER" (pack f.adder) t
+ where
+  t = "(assert (if (and (>= #MIN #OLDID) (< #OLDID #MAX)) (= #NEWID (+ #OLDID #ADDER)) (= #NEWID #OLDID )))"
+  repl old new = Text.replace old new
 
 -- unpackFunc :: Func -> Int -> Int
 -- -- unpackFunc (Func adder min max) x | error $ show adder = undefined
@@ -65,19 +93,31 @@ unpackFuncs = go . sortOn (.min)
   go (Func adder min max : fs) x | x >= min && x < max = x + adder
   go (_ : fs) x = unpackFuncs fs x
 
-solveA (seeds, mappings) = minimum $ map maps seeds
- where
-  maps = foldr1 (.) $ map unpackFuncs $ reverse $ map (map mapToFunc) mappings
+toId i = Text.pack $ "a" ++ show i
 
-getAllSeeds :: [Int] -> [Int]
-getAllSeeds (val : range : rest) = take range [val ..] ++ getAllSeeds rest
-getAllSeeds [] = []
-
-solveB (getAllSeeds -> traceLab "seeds" -> id -> seeds, mappings) = firstJust check [0 ..]
+solveA (seeds, mappings) = Text.unlines $ decls ++ [seedDecl] ++ assertMappings
  where
-  check x | mod x 10000 == 0, traceShow x False = undefined
-  check (maps -> x) = if elem x seeds then Just x else Nothing
-  maps = foldr1 (.) $ map unpackFuncs $ map (map revmapToFunc) mappings
+  funcs = map (map mapToFunc) mappings
+  assertMappings = foldMap id $ imap (\i m -> map (toZ3 (toId i) (toId (i + 1))) m) $ funcs
+
+  decls = imap (\i _ -> declareConst i) $ (funcs ++ [[]])
+
+  seedDecl = "(assert (or " <> ss <> "))"
+  ss = Text.intercalate " " $ map (\x -> "(= a0 " <> pack x <> ")") seeds
+
+declareConst id = "(declare-const " <> (toId id) <> " Int)"
+
+-- maps = foldr1 combMap $ map unpackFuncs $ reverse $ map (map mapToFunc) mappings
+
+-- getAllSeeds :: [Int] -> [Int]
+-- getAllSeeds (val : range : rest) = take range [val ..] ++ getAllSeeds rest
+-- getAllSeeds [] = []
+
+-- solveB (getAllSeeds -> traceLab "seeds" -> id -> seeds, mappings) = firstJust check [minimum (map (.destStart) (last mappings)) ..]
+--  where
+--   check x | mod x 10000 == 0, traceShow x False = undefined
+--   check (maps -> x) = if elem x seeds then Just x else Nothing
+--   maps = foldr1 (.) $ map unpackFuncs $ map (map revmapToFunc) mappings
 
 -- getOutside :: [[Mapping]] -> _
 -- getOutside mappings = IM.fromList $ map (,()) $ map (\m -> IntervalCO m.sourceStart (m.sourceStart + m.range)) $ concat mappings
@@ -127,17 +167,18 @@ run input = void $ do
   print input
   let parsed = parse input
   -- print parsed
-  -- mapM_ print $ snd parsed
+  mapM_ print $ map (foldMap funcToM . map mapToFunc) $ snd parsed
   let resA = solveA parsed
   print "--------------------------------------------"
-  print resA
+  Text.putStrLn resA
+  print "--------------------------------------------"
 
-  -- let f = Func{adder = 70, min = 18, max = 25}
-  -- print $ "\n\n" ++ (show $ unpackFunc f $ 53)
-  -- print $ unpackFunc (mapToFunc $ Mapping 18 25 70) 53
+-- let f = Func{adder = 70, min = 18, max = 25}
+-- print $ "\n\n" ++ (show $ unpackFunc f $ 53)
+-- print $ unpackFunc (mapToFunc $ Mapping 18 25 70) 53
 
-  -- resA @=? 1715
-  let resB = solveB parsed
-  print resB
+-- -- resA @=? 1715
+-- let resB = solveB parsed
+-- print resB
 
 -- resB @=? 1739
