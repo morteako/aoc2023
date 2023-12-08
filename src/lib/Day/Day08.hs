f0726b965cfb3fc3f78b82aa2c307012487517d3
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-08 12:34:09 +0100

    .

---
+++ src/lib/Day/Day08.hs
@@ -0,0 +1,47 @@
+module Day.Day08 (run) where
+
+import Control.Arrow
+import Data.Char (isUpper)
+import Data.List.Extra (findIndex, splitOn)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (fromJust)
+import Data.Set qualified as Set
+import Test.HUnit ((@=?))
+
+data Dir = L | R deriving (Show, Read, Eq, Ord)
+
+parse :: String -> ([Dir], Map ([Char], Dir) [Char], ([[Char]], [[Char]]))
+parse = lines >>> parseDirsAndSteps >>> (\(dirs, Map.fromList -> steps) -> (dirs, steps, getSpecials steps))
+ where
+  parseDirsAndSteps (dirs : "" : steps) = (map (read @Dir . pure) dirs, concatMap parseStep steps)
+  parseStep (splitOn " = " -> [from, to]) = (parseTargets from to)
+  parseTargets fr = filter (flip notElem ("()" :: String)) >>> splitOn ", " >>> makeTups fr
+  makeTups fr [l, r] = [((fr, L), l), ((fr, R), r)]
+
+  getSpecials = Map.keysSet >>> Set.map fst >>> (getMMM 'A' &&& getMMM 'Z')
+
+  getMMM c = Set.filter (\x -> last x == c) >>> Set.toList
+
+countStepsToFromAAAtoZZZ :: ([Dir], Map (String, Dir) String, c) -> Int
+countStepsToFromAAAtoZZZ (dirs, dict, _) = succ $ fromJust $ findIndex (== "ZZZ") (find dict (cycle dirs) "AAA")
+
+find :: Map (String, Dir) String -> [Dir] -> String -> [String]
+find m (d : ds) cur = let next = m Map.! (cur, d) in next : find m ds next
+
+countsStepsToFromAllStartsToEnd :: ([Dir], Map (String, Dir) String, ([String], [String])) -> Integer
+countsStepsToFromAllStartsToEnd (dirs, dict, (starts, Set.fromList -> ends)) = getLcm $ map findOne starts
+ where
+  getLcm = foldr1 lcm . map toInteger
+  isEnd x = Set.member x ends
+  findOne start = succ $ fromJust $ findIndex isEnd (find dict (cycle dirs) start)
+
+run :: String -> IO ()
+run input = do
+  let parsed = parse input
+  let resA = countStepsToFromAAAtoZZZ parsed
+  print resA
+  resA @=? 16531
+  let resB = countsStepsToFromAllStartsToEnd parsed
+  print resB
+  resB @=? 24035773251517
