f4fe48f1efba0a6ab44aecaf529d9c0dd8873770
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-11 19:03:30 +0100

    d11

---
+++ src/lib/Day/Day11.hs
@@ -0,0 +1,36 @@
+module Day.Day11 (run) where
+
+import Control.Arrow ((>>>))
+import Control.Monad (guard)
+import Data.List (transpose)
+import Data.Map qualified as Map
+import Linear (V2 (V2))
+import Test.HUnit ((@=?))
+
+parse :: Integer -> String -> [V2 Integer]
+parse (pred -> repl) = lines >>> addGalaxies >>> Map.fromList >>> Map.filter (== '#') >>> Map.keys
+ where
+  addGalaxies ls = zip (V2 <$> indexRows 0 ls <*> indexRows 0 (transpose ls)) (concat ls)
+
+  indexRows c [] = []
+  indexRows c (x : xs) | all (== '.') x, newC <- c + repl = newC : indexRows (newC + 1) xs
+  indexRows c (x : xs) = (c) : indexRows (c + 1) xs
+
+mandist (V2 x y) (V2 xx yy) = abs (x - xx) + abs (y - yy)
+
+sumDistances :: [V2 Integer] -> Integer
+sumDistances galaxies =
+  sum $ do
+    k <- galaxies
+    k2 <- galaxies
+    guard (k < k2)
+    pure $ mandist k k2
+
+run :: String -> IO ()
+run input = do
+  let resA = sumDistances (parse 2 input)
+  print resA
+  resA @=? 9957702
+  let resB = sumDistances (parse 1000000 input)
+  print resB
+  resB @=? 512240933238
--- src/lib/Solutions.hs
+++ src/lib/Solutions.hs
@@ -2,30 +2,33 @@ module Solutions where
 
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Day.Day01 qualified as Day01
-import Day.Day02 qualified as Day02
-import Day.Day03 qualified as Day03
-import Day.Day04 qualified as Day04
-import Day.Day05 qualified as Day05
-import Day.Day06 qualified as Day06
-import Day.Day07 qualified as Day07
-import Day.Day08 qualified as Day08
-import Day.Day09 qualified as Day09
-import Day.Day10 qualified as Day10
+import Day.Day01 qualified
+import Day.Day02 qualified
+import Day.Day03 qualified
+import Day.Day04 qualified
+import Day.Day05 qualified
+import Day.Day06 qualified
+import Day.Day07 qualified
+import Day.Day08 qualified
+import Day.Day09 qualified
+import Day.Day10 qualified
+
+import Day.Day11 qualified
 import DayVersion
 import Utils ((=:))
 
 solutions :: Map DayVersion (String -> IO ())
 solutions =
   Map.fromList
-    [ "1" =: Day01.run
-    , "2" =: Day02.run
-    , "3" =: Day03.run
-    , "4" =: Day04.run
-    , "5" =: Day05.run
-    , "6" =: Day06.run
-    , "7" =: Day07.run
-    , "8" =: Day08.run
-    , "9" =: Day09.run
-    , "10" =: Day10.run
+    [ "1" =: Day.Day01.run
+    , "2" =: Day.Day02.run
+    , "3" =: Day.Day03.run
+    , "4" =: Day.Day04.run
+    , "5" =: Day.Day05.run
+    , "6" =: Day.Day06.run
+    , "7" =: Day.Day07.run
+    , "8" =: Day.Day08.run
+    , "9" =: Day.Day09.run
+    , "10" =: Day.Day10.run
+    , "11" =: Day.Day11.run
     ]
