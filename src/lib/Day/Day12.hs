51e6748144dfc00525c58fa67c2b4351402458dc
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-12 21:56:01 +0100

    working 12.2, slow

--- src/lib/Day/Day12.hs
+++ src/lib/Day/Day12.hs
@@ -1,19 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day.Day12 (run) where
 
 import Control.Arrow (Arrow (first), (>>>))
 import Control.Monad (void)
-import Data.Function
+import Data.Function.Memoize
 import Data.List
 import Data.List.Extra (splitOn, sumOn')
-import Data.Monoid
-import Debug.Trace
 import Test.HUnit ((@=?))
-import Text.RawString.QQ (r)
-import Utils
 
 data Rec = Op | Dmg | Unk deriving (Eq)
-
--- pattern
 
 instance Show Rec where
   show x = case x of
@@ -25,13 +21,7 @@ toRec '?' = Unk
 toRec '#' = Dmg
 toRec '.' = Op
 
-data Tile = Stuck Int | Split | Un deriving (Eq, Read)
-
-instance Show Tile where
-  show x = case x of
-    Split -> "."
-    Stuck _ -> "#"
-    Un -> "?"
+data Tile = Stuck Int | Split | Un deriving (Eq)
 
 parse :: String -> Int -> [([Tile], [Int])]
 parse inp repl = lines >>> fmap parseLine $ inp
@@ -48,87 +38,42 @@ parse inp repl = lines >>> fmap parseLine $ inp
 
   pNums = splitOn "," >>> map (read @Int)
 
-makeCombs :: [Maybe Int] -> _
-makeCombs = traverse (maybe [(0, False), (1, False)] (pure . (,True)))
+deriveMemoizable ''Tile
 
-debug = False
-
-traceLabd s a = if debug then traceLab s a else a
-traceShowd s a = if debug then traceShow s a else a
-
--- doRec c gs tiles
--- \| traceShowd (c, gs, tiles) False =
---     undefined
-doRec c (g : _) _
-  | c > g = 0
-doRec c (curGoals : goals) (Split : tiles)
-  | c == 0 = doRec 0 (curGoals : goals) tiles
-  | c == curGoals = doRec 0 goals tiles
-  | otherwise = 0
-doRec c goals (Stuck i : tiles) =
-  doRec (c + i) goals tiles
-doRec (id -> c) (curGoal : goals) (Un : tiles)
-  | c == 0 = traceShowd (Un : tiles) useStuck + useSplit
-  | c == curGoal = doRec 0 goals tiles
-  | c < curGoal = useStuck
-  | otherwise = traceShowd "WTF" 0 --
+countCombinations :: [Int] -> [Tile] -> Integer
+countCombinations = memDoRec 0
  where
-  useStuck = traceLabd "stuck" $ doRec (c + 1) (curGoal : goals) tiles
-  useSplit = traceLabd "split" $ doRec c (curGoal : goals) tiles
--- doRec c curG
-doRec 0 [] tiles | all (\x -> x == Un || x == Split) tiles = 1
-doRec 0 [] [] = 1
-doRec c [g] [] | c == g = 1
-doRec c gs ts = 0 -- & traceShowd (gs, ts)
+  memDoRec = memoize3 doRec
 
--- doRec (curGoal : goals) (Stuck i : Un : tiles)
--- doRec [curGoal] [Stuck i] =
-solveA repl inp = do
-  let newInp = inp repl
-  print "res"
-  let res = sum $ fmap f $ newInp
-  print $ res
- where
-  f (tiles, goals) = trace ":)" doRec 0 goals tiles
+  doRec c (g : _) _
+    | c > g = 0
+  doRec c (curGoals : goals) (Split : tiles)
+    | c == 0 = memDoRec 0 (curGoals : goals) tiles
+    | c == curGoals = memDoRec 0 goals tiles
+    | otherwise = 0
+  doRec c goals (Stuck i : tiles) =
+    memDoRec (c + i) goals tiles
+  doRec (id -> c) (curGoal : goals) (Un : tiles)
+    | c == 0 = useStuck + useSplit
+    | c == curGoal = memDoRec 0 goals tiles
+    | c < curGoal = useStuck
+   where
+    useStuck = memDoRec (c + 1) (curGoal : goals) tiles
+    useSplit = memDoRec c (curGoal : goals) tiles
+  doRec 0 [] tiles | all (\x -> x == Un || x == Split) tiles = 1
+  doRec c [g] [] | c == g = 1
+  doRec _ _ _ = 0
 
--- check2 (nums, goal) = getRes nums
---   where
---     numSplits = count Nothing nums
--- check (nums, goal) = foldMap (\x -> if x == goal then Sum 1 else 0) $ getRes nums
--- getRes = makeCombs >>> fmap (group >>> traceShowdId >>> fmap (sumOn' fst) >>> filter (> 0))
-
-solveB = id
-
-testInput =
-  (!! 0) $
-    [r|???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1
-|]
-      : [r|?.### 1,3
-|]
-      : []
+solveA :: Int -> (Int -> [([Tile], [Int])]) -> Integer
+solveA repl inp = sumOn' (\(tiles, goals) -> countCombinations goals tiles) $ inp repl
 
 run :: String -> IO ()
 run input = void $ do
-  -- input <- putStrLn "#####    testInput   #####" >> pure testInput
-  print input
   let parsed = parse input
-  mprint $ parsed 1
+  let resA = solveA 1 parsed
+  print resA
+  resA @=? 8270
 
-  let resA = solveA 5 parsed
-  resA
-
--- mapM_ (print . concatMap show . fst) $ parsed 5
--- mapM_ (print . snd) $ parsed 5
-
--- let resB = solveA 2 parsed
--- resB
-
--- resA @=? 1715
--- let resB = solveB parsed
+-- let resB = solveA 5 parsed
 -- print resB
--- resB @=? 1739
+-- resB @=? 204640299929836
