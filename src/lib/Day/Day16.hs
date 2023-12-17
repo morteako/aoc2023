4d01ff74a99642c37f43a09c87d74c011f8d924d
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-17 02:22:25 +0100

    refac

--- src/lib/Day/Day16.hs
+++ src/lib/Day/Day16.hs
@@ -2,30 +2,20 @@ module Day.Day16 (run) where
 
 import Control.Applicative qualified as Set
 import Control.Arrow ((>>>))
-import Control.Monad (void)
 import Control.Monad.State
 import Data.Foldable
 
-import Data.Map qualified as Map hiding (filter)
-import Data.Semigroup
+import Data.Map.Strict qualified as Map hiding (filter)
+import Data.Semigroup (Max (Max), Min (Min))
 
-import Data.Map (Map)
+import Data.Map.Strict (Map)
 import Data.Set (Set)
 import Data.Set qualified as Set hiding (filter)
-import Data.Traversable
-import Linear hiding (E)
+import Linear (V2 (..))
 import Test.HUnit ((@=?))
-import Text.RawString.QQ (r)
-import Utils
+import Utils (parseAsciiMap)
 
 data Tile = Dot | Vert | Hor | Refl_V | ReflV_
-
-instance Show Tile where
-  show Dot = "."
-  show Vert = "|"
-  show Hor = "-"
-  show Refl_V = "\\"
-  show ReflV_ = "/"
 
 type Grid = Map (V2 Int) Tile
 
@@ -48,34 +38,29 @@ dirToVec x = case x of
   E -> V2 1 0
   W -> V2 (-1) 0
 
-pattern NE d <- ((\x -> if x == N || x == E then Just x else Nothing) -> Just d)
-pattern SW d <- ((\x -> if x == S || x == W then Just x else Nothing) -> Just d)
-
-pattern J x = Just x
-
+isOne :: (Eq a) => [a] -> a -> Maybe a
 isOne xs x = if elem x xs then Just x else Nothing
 
--- /
-reflV_ dir = case dir of
-  N -> E
-  E -> N
-  W -> S
-  S -> W
-
--- \
-refl_V dir = case dir of
-  N -> W
-  W -> N
-  E -> S
-  S -> E
-
+move :: Tile -> Dir -> [Dir]
 move Dot dir = [dir]
-move Vert (isOne [N, S] -> J dir) = [dir]
-move Hor (isOne [E, W] -> J dir) = [dir]
-move Vert (isOne [E, W] -> J dir) = [N, S]
-move Hor (isOne [N, S] -> J dir) = [E, W]
+move Vert (isOne [N, S] -> Just dir) = [dir]
+move Hor (isOne [E, W] -> Just dir) = [dir]
+move Vert (isOne [E, W] -> Just dir) = [N, S]
+move Hor (isOne [N, S] -> Just dir) = [E, W]
 move ReflV_ dir = [reflV_ dir]
+ where
+  reflV_ dir = case dir of
+    N -> E
+    E -> N
+    W -> S
+    S -> W
 move Refl_V dir = [refl_V dir]
+ where
+  refl_V dir = case dir of
+    N -> W
+    W -> N
+    E -> S
+    S -> E
 
 doMoves :: Map (V2 Int) Tile -> V2 Int -> Dir -> State (Set (V2 Int, Dir)) ()
 doMoves grid curPos dir = case Map.lookup curPos grid of
@@ -102,34 +87,12 @@ edgesAndDirs grid =
   fi p dir = map (,dir) $ filter p $ Map.keys grid
   (Min mx, Max mxx, Min my, Max myy) = Map.foldMapWithKey (\(V2 x y) _ -> (Min x, Max x, Min y, Max y)) grid
 
-doEdgeMoves ::
-  Map (V2 Int) Tile ->
-  Set (V2 Int) ->
-  V2 Int ->
-  Dir ->
-  State (Map (V2 Int, Dir) (Set (V2 Int))) (Set (V2 Int))
-doEdgeMoves grid curs curPos dir = case Map.lookup curPos grid of
-  Nothing -> pure curs
-  Just t -> do
-    seen <- get
-    case Map.lookup (curPos, dir) seen of
-      Nothing -> do
-        modify (Map.insertWith (<>) (curPos, dir) curs)
-        let ds = move t dir
-        reaches <- for ds $ \d -> doEdgeMoves grid (Set.insert (dirToVec d + curPos) curs) (dirToVec d + curPos) d
-        pure $ Set.unions reaches
-      Just canReach -> do
-        let news = (canReach <> curs)
-        modify (Map.insert (curPos, dir) news)
-        pure news
-
+solveB :: Map (V2 Int) Tile -> Int
 solveB grid =
   maximum
     . map (Set.size . Set.map fst)
-    $ flip map es
+    $ flip map (edgesAndDirs grid)
     $ flip execState mempty . uncurry (doMoves grid)
- where
-  es = edgesAndDirs grid
 
 run :: String -> IO ()
 run input = do
