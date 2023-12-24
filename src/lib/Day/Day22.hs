41f4a2afcc3761e93ed99902bfbdd5748fd0ff6f
Morten Kolstad <morten.kolstad@bekk.no>
2023-12-24 14:04:23 +0100

    fix

--- src/lib/Day/Day22.hs
+++ src/lib/Day/Day22.hs
@@ -1,10 +1,8 @@
 module Day.Day22 (run) where
 
 import Control.Arrow ((>>>))
-import Control.Lens hiding (un)
-import Control.Monad (void)
+import Control.Lens
 import Control.Monad.State
-import Data.Either (partitionEithers)
 import Data.Foldable
 import Data.List
 import Data.List.Extra (groupOn, splitOn)
@@ -34,20 +32,22 @@ points (a@(V3 x y z), b@(V3 xx yy zz))
 
 type Grid = Map (V3 Int) Char
 
-getMoved labeledIntervals = (moveAll grid, labs)
+getMoved labeledIntervals = (moveAll grid, Set.toList labs)
  where
   labeledPoints = over (each . _2) points labeledIntervals
 
-  grid = Map.fromListWith undefined $ concatMap (\(lab, ps) -> map (,lab) ps) labeledPoints
+  grid = Map.fromList $ concatMap (\(lab, ps) -> map (,lab) ps) labeledPoints
 
   labs = Set.fromList $ map fst labeledIntervals
 
+solveA :: [(Char, (V3 Int, V3 Int))] -> Int
 solveA (getMoved -> (moved, labs)) = do
-  let cantBeMoved = Set.unions $ filter (\x -> Set.size x <= 1) $ map (flip checkRemove moved) $ Set.toList labs
-  Set.size labs - Set.size cantBeMoved
+  let cantBeMoved = Set.unions $ filter (\x -> Set.size x <= 1) $ map (flip checkRemove moved) $ labs
+  length labs - Set.size cantBeMoved
 
+solveB :: [(Char, (V3 Int, V3 Int))] -> Int
 solveB (getMoved -> (moved, labs)) = do
-  let deps = Map.filter (not . Set.null) $ Map.fromList $ map (\l -> (l, checkRemove l moved)) $ Set.toList labs
+  let deps = Map.filter (not . Set.null) $ Map.fromList $ map (\l -> (l, checkRemove l moved)) $ labs
   sum $ map length $ map (\l -> flip evalState deps (getAlls l)) (toList labs)
 
 getAlls :: Char -> _
@@ -71,21 +71,22 @@ checkRemove lab grid =
 moveDown :: V3 Int -> V3 Int
 moveDown = over _z pred
 
-moveLabDown :: Char -> Grid -> _
-moveLabDown (lab :: Char) grid
-  | any ((< 1) . view _z) $ Map.keys movedPoints = grid
-  | or (Map.intersectionWith (/=) movedPoints grid) = grid
-  | otherwise = moveLabDown lab (movedPoints <> Map.difference grid ls) -- optimize??
- where
-  movedPoints = Map.mapKeys moveDown $ ls
-  ls = Map.filter (== lab) grid
-
+moveAll :: Grid -> Grid
 moveAll grid = foldl' (\g l -> moveLabDown l g) grid ps
  where
   ps = map head . group . map snd $ sortOn (\(V3 x y z, _) -> (z, y, x)) $ Map.toList grid
 
+moveLabDown :: Char -> Grid -> Grid
+moveLabDown (lab :: Char) grid
+  | any ((< 1) . view _z) $ Map.keys movedPoints = grid
+  | or (Map.intersectionWith (/=) movedPoints grid) = grid
+  | otherwise = moveLabDown lab (movedPoints <> Map.difference grid ls)
+ where
+  movedPoints = Map.mapKeys moveDown $ ls
+  ls = Map.filter (== lab) grid
+
 run :: String -> IO ()
-run input = void $ do
+run input = do
   let parsed = parse input
 
   let resA = solveA parsed
